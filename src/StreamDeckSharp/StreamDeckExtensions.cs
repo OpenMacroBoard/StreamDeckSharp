f28dcd8d0c37eb63b1eefb9ca1cf17d3dd92f63b
Christian Wischenbart <c.wischenbart@bitoxide.com>
2017-06-29 14:19:07 +0200

    Add extension method to set bitmap for all keys (+Xml documentation)

--- src/StreamDeckSharp/StreamDeckExtensions.cs
+++ src/StreamDeckSharp/StreamDeckExtensions.cs
@@ -17,20 +17,45 @@ namespace StreamDeckSharp
     /// </remarks>
     public static class StreamDeckExtensions
     {
+        /// <summary>
+        /// Sets a background image for a given key
+        /// </summary>
+        /// <param name="deck"></param>
+        /// <param name="keyId"></param>
+        /// <param name="bitmap"></param>
         public static void SetKeyBitmap(this IStreamDeck deck, int keyId, StreamDeckKeyBitmap bitmap)
         {
             deck.SetKeyBitmap(keyId, bitmap.rawBitmapData);
         }
 
+        /// <summary>
+        /// Sets a background image for all keys
+        /// </summary>
+        /// <param name="deck"></param>
+        /// <param name="bitmap"></param>
+        public static void SetKeyBitmap(this IStreamDeck deck, StreamDeckKeyBitmap bitmap)
+        {
+            for (int i = 0; i < StreamDeckHID.numOfKeys; i++)
+                deck.SetKeyBitmap(i, bitmap.rawBitmapData);
+        }
+
+        /// <summary>
+        /// Sets background to black for a given key
+        /// </summary>
+        /// <param name="deck"></param>
+        /// <param name="keyId"></param>
         public static void ClearKey(this IStreamDeck deck, int keyId)
         {
             deck.SetKeyBitmap(keyId, StreamDeckKeyBitmap.Black);
         }
 
+        /// <summary>
+        /// Sets background to black for all given keys
+        /// </summary>
+        /// <param name="deck"></param>
         public static void ClearKeys(this IStreamDeck deck)
         {
-            for (int i = 0; i < StreamDeckHID.numOfKeys; i++)
-                deck.SetKeyBitmap(i, StreamDeckKeyBitmap.Black);
+            deck.SetKeyBitmap(StreamDeckKeyBitmap.Black);
         }
     }
 }
