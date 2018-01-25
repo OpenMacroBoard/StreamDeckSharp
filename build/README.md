f3f78138bcc4bd37d1bf12707ba9d23ab6d0de56
Christian Wischenbart <c.wischenbart@bitoxide.com>
2018-01-25 02:12:28 +0100

    Add some simple build scripts

---
+++ build/README.md
@@ -0,0 +1,1 @@
+Just some quick and dirty build scripts (for windows)
---
+++ build/create-nugetpackage.bat
@@ -0,0 +1,2 @@
+@echo off
+nuget.exe pack ..\src\StreamDeckSharp\StreamDeckSharp.nuspec -OutputDirectory "..\src\StreamDeckSharp\bin\Release\Merged"
