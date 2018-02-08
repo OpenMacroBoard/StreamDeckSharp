2399bed80b2e793d240aec96c702b9c5515de05b
Christian Wischenbart <c.wischenbart@bitoxide.com>
2018-02-08 21:54:18 +0100

    Remove old nuget packages

--- build/repack-release.bat
+++ build/repack-release.bat
@@ -1,2 +1,2 @@
 @echo off
-..\src\packages\ILRepack.2.0.13\tools\ILRepack.exe /out:"..\src\StreamDeckSharp\bin\Release\Merged\StreamDeckSharp.dll" /xmldocs /internalize "..\src\StreamDeckSharp\bin\Release\StreamDeckSharp.dll" "..\src\StreamDeckSharp\bin\Release\HidLibrary.dll"
+ILRepack.exe /out:"..\src\StreamDeckSharp\bin\Release\Merged\StreamDeckSharp.dll" /xmldocs /internalize "..\src\StreamDeckSharp\bin\Release\StreamDeckSharp.dll" "..\src\StreamDeckSharp\bin\Release\HidLibrary.dll"
