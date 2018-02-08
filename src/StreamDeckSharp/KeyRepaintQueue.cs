13848a033f6c598df707ac61fd2ccae03a6a32e8
Christian Wischenbart <c.wischenbart@bitoxide.com>
2018-02-09 00:20:31 +0100

    Remove very long comment
    + added info to github wiki

--- src/StreamDeckSharp/KeyRepaintQueue.cs
+++ src/StreamDeckSharp/KeyRepaintQueue.cs
@@ -25,24 +25,8 @@ namespace StreamDeckSharp
 
         /// <summary>
         /// Value choosen after many experiments ;-)
+        /// See github wiki...
         /// </summary>
-        /// <remarks>
-        /// If we write as fast as possible to the StreamDeck, glitches start to appear.
-        /// The StreamDeck couldn't keep up and (I guess) startet to write over
-        /// buffers that are still used to draw other images.
-        /// 
-        /// Rewrote the RepaintQueue to be lock-free and to make sure every key has enough
-        /// time to "cool down" ;-)
-        /// 
-        /// I did some experiments with different values (rendered a video "fullscreen")
-        /// 50ms ( 20 FPS) caused glitches after about 10 seconds
-        /// 65ms (~15 FPS) worked for about 30 seconds
-        /// 70ms (~14 FPS) never saw a glitch
-        /// 
-        /// so I decided to use 75ms (~ 13 FPS) just to be on the safe side
-        /// and 13 FPS should be enough for most applications
-        /// (the video still looks fine)
-        /// </remarks>
         private const int keyCooldownTime = 75;//ms
 
         public KeyRepaintQueue(CancellationToken token)
