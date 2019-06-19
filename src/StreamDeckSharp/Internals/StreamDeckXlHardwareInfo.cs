a9cd764b8832ee578db9e8319975942427f05b6f
Christian Wischenbart <c.wischenbart@bitoxide.com>
2019-06-19 16:55:39 +0200

    Move jpg encoding to separate method
    and support null value KeyBitmaps.

--- src/StreamDeckSharp/Internals/StreamDeckXlHardwareInfo.cs
+++ src/StreamDeckSharp/Internals/StreamDeckXlHardwareInfo.cs
@@ -25,6 +25,7 @@ namespace StreamDeckSharp.Internals
             => keyPositions;
 
         private static readonly GridKeyPositionCollection keyPositions;
+        private static byte[] cachedNullImage = null;
 
         static StreamDeckXlHardwareInfo()
         {
@@ -38,33 +39,10 @@ namespace StreamDeckSharp.Internals
         {
             var rawData = keyBitmap.GetScaledVersion(imgWidth, imgWidth);
 
-            int stride = ((imgWidth * 32 + 31) & ~31) / 8;
-            var data = new byte[imgWidth * imgWidth * 4];
+            if (rawData is null)
+                return GetNullImage();
 
-            for (int y = 0; y < imgWidth; y++)
-                for (int x = 0; x < imgWidth; x++)
-                {
-                    var x1 = imgWidth - 1 - x;
-                    var y1 = imgWidth - 1 - y;
-
-                    var pTarget = (y * imgWidth + x) * 4;
-                    var pSource = (y1 * imgWidth + x1) * 3;
-
-                    data[pTarget + 2] = rawData[pSource + 2];
-                    data[pTarget + 1] = rawData[pSource + 1];
-                    data[pTarget + 0] = rawData[pSource + 0];
-                }
-
-            var enc = new JpegBitmapEncoder() { QualityLevel = 100 };
-            var f = BitmapSource.Create(imgWidth, imgWidth, 96, 96, PixelFormats.Bgr32, null, data, stride);
-            enc.Frames.Add(BitmapFrame.Create(f));
-
-            using (var ms = new MemoryStream())
-            {
-                enc.Save(ms);
-                var jpgBytes = ms.ToArray();
-                return jpgBytes;
-            }
+            return EncodeImageToJpg(rawData);
         }
 
         public int HardwareKeyIdToExtKeyId(int hardwareKeyId)
@@ -79,6 +57,48 @@ namespace StreamDeckSharp.Internals
             data[4] = (byte)(payloadLength & 255);
             data[5] = (byte)(payloadLength >> 8);
             data[6] = (byte)pageNumber;
+        }
+
+        private static byte[] GetNullImage()
+        {
+            if (cachedNullImage is null)
+            {
+                var rawNullImg = new KeyBitmap(1, 1, new byte[] { 0, 0, 0 }).GetScaledVersion(imgWidth, imgWidth);
+                cachedNullImage = EncodeImageToJpg(rawNullImg);
+            }
+
+            return cachedNullImage;
+        }
+
+        private static byte[] EncodeImageToJpg(byte[] rgb24)
+        {
+            int stride = imgWidth * 4;
+            var data = new byte[imgWidth * stride];
+
+            for (int y = 0; y < imgWidth; y++)
+                for (int x = 0; x < imgWidth; x++)
+                {
+                    var x1 = imgWidth - 1 - x;
+                    var y1 = imgWidth - 1 - y;
+
+                    var pTarget = (y * imgWidth + x) * 4;
+                    var pSource = (y1 * imgWidth + x1) * 3;
+
+                    data[pTarget + 0] = rgb24[pSource + 0];
+                    data[pTarget + 1] = rgb24[pSource + 1];
+                    data[pTarget + 2] = rgb24[pSource + 2];
+                }
+
+            var enc = new JpegBitmapEncoder() { QualityLevel = 100 };
+            var f = BitmapSource.Create(imgWidth, imgWidth, 96, 96, PixelFormats.Bgr32, null, data, stride);
+            enc.Frames.Add(BitmapFrame.Create(f));
+
+            using (var ms = new MemoryStream())
+            {
+                enc.Save(ms);
+                var jpgBytes = ms.ToArray();
+                return jpgBytes;
+            }
         }
     }
 }
