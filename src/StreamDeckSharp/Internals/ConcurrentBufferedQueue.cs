2027e81ea116ed7171629781789672b85c878393
Christian Wischenbart <christian.wischenbart@gmail.com>
2019-11-28 22:58:58 +0100

    Remove Bcl dependency
    because it cause problems with indirect dependencies

--- src/StreamDeckSharp/Internals/ConcurrentBufferedQueue.cs
+++ src/StreamDeckSharp/Internals/ConcurrentBufferedQueue.cs
@@ -165,12 +165,24 @@ namespace StreamDeckSharp.Internals
             }
         }
 
+#if NET40
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "Ignore timer dispose, happens during finalize")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0067:Dispose objects before losing scope", Justification = "Ignore timer dispose, happens during finalize")]
+        private static Task DelayNet40(int milliseconds)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            new Timer(_ => tcs.SetResult(null)).Change(milliseconds, -1);
+            return tcs.Task;
+        }
+#endif
+
         private static Task Delay(long milliseconds)
         {
 #if NETSTANDARD2_0
             return Task.Delay((int)milliseconds);
 #else
-            return TaskEx.Delay((int)milliseconds);
+
+            return DelayNet40((int)milliseconds);
 #endif
         }
 
