d393b13a27d4fd3c0dcadb8a626a969a19ae3f3f
Christian Wischenbart <christian.wischenbart@gmail.com>
2021-03-05 00:35:42 +0100

    Use new IKeyDataAccess.GetBitmap feature

--- src/StreamDeckSharp/Internals/KeyBitmapExtensions.cs
+++ src/StreamDeckSharp/Internals/KeyBitmapExtensions.cs
@@ -18,46 +18,16 @@ namespace StreamDeckSharp.Internals
                 return null;
             }
 
-            var rawData = new byte[keyDataAccess.DataLength];
-            keyDataAccess.CopyData(rawData, 0);
-
             if (keyBitmap.Width == width && keyBitmap.Height == height)
             {
-                return rawData;
+                return keyDataAccess.CopyData();
             }
 
             var destRect = new Rectangle(0, 0, width, height);
 
             using (var scaledBmp = new Bitmap(width, height, PixelFormat.Format24bppRgb))
-            using (var sourceBmp = new Bitmap(keyBitmap.Width, keyBitmap.Height, PixelFormat.Format24bppRgb))
+            using (var sourceBmp = keyDataAccess.GetBitmap())
             {
-                var bmpData = sourceBmp.LockBits(new Rectangle(0, 0, sourceBmp.Width, sourceBmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
-                try
-                {
-                    var h = sourceBmp.Height;
-                    var w = sourceBmp.Width;
-
-                    var alignedData = new byte[bmpData.Stride * h];
-
-                    for (var y = 0; y < h; y++)
-                    {
-                        for (var x = 0; x < w; x++)
-                        {
-                            var ps = bmpData.Stride * y + x * 3;
-                            var pt = (w * y + x) * 3;
-                            alignedData[ps + 0] = rawData[pt + 0];
-                            alignedData[ps + 1] = rawData[pt + 1];
-                            alignedData[ps + 2] = rawData[pt + 2];
-                        }
-                    }
-
-                    Marshal.Copy(alignedData, 0, bmpData.Scan0, rawData.Length);
-                }
-                finally
-                {
-                    sourceBmp.UnlockBits(bmpData);
-                }
-
                 using (var g = Graphics.FromImage(scaledBmp))
                 {
                     g.CompositingMode = CompositingMode.SourceCopy;
@@ -73,7 +43,7 @@ namespace StreamDeckSharp.Internals
                     }
                 }
 
-                bmpData = scaledBmp.LockBits(destRect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+                var bmpData = scaledBmp.LockBits(destRect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
                 try
                 {
                     var dataArray = new byte[width * height * 3];
